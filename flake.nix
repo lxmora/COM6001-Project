fae3cb5e3cc3dd596db3e9c67df44517ee4236fb
Logan Morazain <lxmorazain@gmail.com>
2025-10-23 10:09:50 +0100

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Basic C++ development flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... } @ inputs: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  { 
+    devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ 
+          neovim
+          gcc
+          cmake
+          ];
+      };
+  };
+}
